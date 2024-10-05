68b287c54e17bf83844df5b047814388916d7d83
zor-cell <philippemanu.zorman@gmail.com>
2024-10-05 12:23:31 +0200

    fixed total timer bar tim display

--- frontend/src/app/components/total-timer/total-timer.component.ts
+++ frontend/src/app/components/total-timer/total-timer.component.ts
@@ -1,14 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-total-timer',
   templateUrl: './total-timer.component.html',
   styleUrl: './total-timer.component.css'
 })
-export class TotalTimerComponent {
-  //the percentage of the timer bar that is always shown
-  readonly offset: number = 10;
-
+export class TotalTimerComponent implements AfterViewInit {
   private _color: string = 'rgba(0, 0, 0, 0)';
   @Input() set color(value: string) {
     this._color = value;
@@ -29,6 +26,10 @@ export class TotalTimerComponent {
   @Input() maxSeconds!: number;
   @Output() timerFullEvent = new EventEmitter<number>;
 
+  @ViewChild('timeDisplay') timeDisplay!: ElementRef;
+  @ViewChild('timerContainer') timerContainer!: ElementRef;
+  percentOffset: number = 0;
+
   timerIsFull: boolean = false;
   secondsPassed: number = 0;
 
@@ -36,18 +37,31 @@ export class TotalTimerComponent {
   //increment 
   increment: number = 0;
 
-  get currentPercent() {
-    if(!this.maxSeconds) return 0;
+  get calcWidth() {
+    if(!this.maxSeconds) return `${this.percentOffset}px`;
 
     if(!this.timerIsFull && this.secondsPassed >= this.maxSeconds) {
       this.timerFullEvent.emit(this.totalTimerId);
       this.timerIsFull = true;
     }
 
-    return this.offset + Math.min(this.secondsPassed / this.maxSeconds, 1) * (100 - this.offset);
+    return `calc(${this.percentOffset}% + ${Math.min(this.secondsPassed / this.maxSeconds, 1) * (100 - this.percentOffset)}%)`;
   }
   
-  constructor() {}
+  constructor(private ref: ElementRef, private cdr: ChangeDetectorRef) {}
 
+  ngAfterViewInit(): void {  
+    this.updatePercentOffset();
+  }
 
+  @HostListener('window:resize', ['$event']) 
+  onResize(event:any) {
+    this.updatePercentOffset();
+  }
+
+  private updatePercentOffset(): void {
+    this.percentOffset = (this.timeDisplay.nativeElement.offsetWidth / this.ref.nativeElement.offsetWidth) * 100;
+
+    this.cdr.detectChanges();
+  }
 }
